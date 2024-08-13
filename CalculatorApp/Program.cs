b16572553bc8a02afe6a54e53dff4fc5c0665389
Stanislav Frinca <stanislav.frinca@toluna.com>
2024-08-13 17:14:36 +0300

    added looping logic

--- CalculatorApp/Program.cs
+++ CalculatorApp/Program.cs
@@ -1,47 +1,64 @@
-﻿
-double aVal, bVal, result;
+﻿bool running = true;
 
-Console.WriteLine("-----------------");
-Console.WriteLine("Input your numbers");
-Console.WriteLine("a=");
-string a = Console.ReadLine();
+while (running == true)
+{   try
+    {
+        double aVal, bVal, result;
 
-Console.WriteLine("b=");
-string b = Console.ReadLine();
+        Console.WriteLine("-----------------");
+        Console.WriteLine("Input your numbers");
+        Console.WriteLine("a=");
+        string a = Console.ReadLine();
 
-if (double.TryParse(a, out aVal) == false | double.TryParse(b, out bVal) == false )
-    { Console.WriteLine("invalid input");
-    return;
+        Console.WriteLine("b=");
+        string b = Console.ReadLine();
+
+        if (double.TryParse(a, out aVal) == false || double.TryParse(b, out bVal) == false)
+        {
+            Console.WriteLine("invalid input");
+            continue;
+        }
+
+
+        Console.WriteLine("Select operation: +,-,*,/,^ ");
+        string op = Console.ReadLine();
+
+        switch (op)
+        {
+            case "+":
+                result = AddOp(aVal, bVal);
+                break;
+            case "-":
+                result = SubtractOp(aVal, bVal);
+                break;
+            case "*":
+                result = MultiplyOp(aVal, bVal);
+                break;
+            case "/":
+                result = DivideOp(aVal, bVal);
+                break;
+            case "^":
+                result = PowerOp(aVal, bVal);
+                break;
+            default:
+                Console.WriteLine("Invalid operation selected");
+                result = double.NaN;
+                break;
+        }
+        result = Math.Round(result);
+        Console.WriteLine("the result of the {0} {1} {2} operation is {3}", a, op, b, result);
+        Console.WriteLine("do you wish to continue? (Y/n)");
+        string decision = Console.ReadLine();
+        if(decision == "n")
+            running = false;
+
+    }
+    catch(Exception e)
+    {
+
+    }
 }
 
-
-Console.WriteLine("Select operation: +,-,*,/,^ ");
-string op = Console.ReadLine();
-
-switch (op)
-{
-        case "+":
-            result = AddOp(aVal,bVal);
-            break;
-        case "-":
-            result = SubtractOp(aVal,bVal);
-            break;
-        case "*":
-            result = MultiplyOp(aVal,bVal);
-            break;
-        case "/":
-            result = DivideOp(aVal,bVal);
-            break;
-        case "^":
-            result = PowerOp(aVal,bVal);
-            break;
-default:
-        Console.WriteLine("Invalid operation selected");
-        result = double.NaN;
-        break;
-}
-result = Math.Round(result);
-Console.WriteLine("the result of the {0} {1} {2} operation is {3}",a,op,b, result);
 
 double AddOp(double a, double b)
 {
@@ -62,7 +79,8 @@ double DivideOp(double a, double b)
 {
     if( b == 0)
     {
-        throw new DivideByZeroException("Division by 0 is not valid");
+        Console.WriteLine("Can't divide by 0");
+        return double.NaN;
     }
     return a / b;
 }
